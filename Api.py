0f259c0ed6423419a4a6579c2d68b13607d6b677
Skinkeve <Martinhansen4780@gmail.com>
2020-02-17 14:56:08 +0100

    At hente en api

--- Api.py
+++ Api.py
@@ -1,13 +1,20 @@
 import json
+import requests
 
 class GetArticle():
     pass
 
 
 class Structure():
-    def Main():
-        pass
+    def Main(self):
+        response = requests.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=yourkey")
+        print(response)
 
 
 class Translate():
     pass
+
+#https://www.dataquest.io/blog/python-api-tutorial/
+
+start = Structure()
+start.Main()
